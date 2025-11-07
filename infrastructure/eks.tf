d157407ca5afe4432969e0afa1484b3a68555e38
abhishekjha <abhishek1.jha@gmail.com>
2025-11-07 16:38:07 -0600

    initial commit

---
+++ infrastructure/eks.tf
@@ -0,0 +1,81 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 21.0"
+
+  name               = "my-eks-cluster"
+  kubernetes_version = "1.31"
+
+  # EKS Addons
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+  endpoint_public_access  = true # Keep it false for PROD env.
+  endpoint_private_access = true # This is used in cluster hardening.
+  #endpoint_public_access_cidrs = ["X.X.X.X/32"]
+  enable_cluster_creator_admin_permissions = true 
+  
+
+
+  eks_managed_node_groups = {
+    node_group_1 = {
+      instance_types = ["t3a.medium"]
+      #ami_type       = "AL2023_x86_64_STANDARD"
+
+      min_size = 1
+      max_size = 3
+      desired_size = 1
+      capacity_type  = "ON_DEMAND"
+    }
+  }
+  tags = local.tags
+}
+
+# Create local kubeconfig 
+
+data "aws_caller_identity" "current" {}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+  depends_on = [module.eks]
+}
+
+provider "kubernetes" {
+  host = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token = data.aws_eks_cluster_auth.cluster.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token = data.aws_eks_cluster_auth.cluster.token
+  }
+}
+#INSTALL INGRESS-NGINX
+
+resource "helm_release" "ingress-nginx" {
+  name = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
+  namespace = "ingress-nginx"
+  create_namespace = true
+  version = "4.13.3"
+
+  depends_on = [module.eks]
+}
